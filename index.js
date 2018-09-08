8b8ad712487445717cb5340916f5aedbc1f8de70
JoWos <woszczyk.joanna@gmail.com>
2018-09-08 19:59:47 +0200

    Add reading from static files

--- index.js
+++ index.js
@@ -5,14 +5,16 @@ var server = http.createServer();
 server.on('request', function (request, response) {
     response.setHeader("Content-Type", "text/html; charset=utf-8");
     if (request.method === 'GET' && request.url === '/') {
-        response.write('<h1>Hej! Miło Cię widzieć!</h1>');
+        fs.readFile('./index.html', 'utf-8', function(err, data) {
+            response.write(data);
             response.end();
+        });
     } else {
         response.setHeader("Content-Type", "image/png");
         fs.readFile('./404.png', function(err, data) {
             response.write(data);
-            response.end()
-        })
+            response.end();
+        });
     }
 });
 
