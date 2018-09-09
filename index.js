aa7f40ce2ce3229dfb6872e7e5ae3acc7c564120
JoWos <woszczyk.joanna@gmail.com>
2018-09-09 23:24:58 +0200

    Add status code

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var fs = require('fs');
 var server = http.createServer();
 
 server.on('request', function (request, response) {
-    response.setHeader("Content-Type", "text/html; charset=utf-8");
     if (request.method === 'GET' && request.url === '/') {
+        response.setHeader("Content-Type", "text/html; charset=utf-8");
         fs.readFile('./index.html', 'utf-8', function(err, data) {
             response.write(data);
             response.end();
@@ -12,6 +12,7 @@ server.on('request', function (request, response) {
     } else {
         response.setHeader("Content-Type", "image/png");
         fs.readFile('./404.png', function(err, data) {
+            response.statusCode = 404;
             response.write(data);
             response.end();
         });
